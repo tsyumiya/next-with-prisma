aece67d4c0ae0a4906bb37a7300564c6f908106c
TY <thales.y@gmail.com>
2025-04-01 10:47:57 -0300

    removed cache from page

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -3,24 +3,11 @@ import { prisma } from "@/app/lib/db"
 import { cache } from "react"
 
 export default async function PostPage({ params }: { params: { slug: string } }) {
-  const { slug } = await params
-  // const post = await prisma.post.findUnique({
-  //   where: { slug },
-  //   include: { author: true }
-  // })
-
-  const getPost = cache(async (slug: string) => {
-    const post = await prisma.post.findUnique({
-      where: { slug },
-      include: {
-        author: true
-      }
-    })
-
-    return post
+  const { slug } = params
+  const post = await prisma.post.findUnique({
+    where: { slug },
+    include: { author: true }
   })
-
-  const post = await getPost(slug)
 
   return (
     <main className="flex flex-col items-center gap-y-5 pt-24">
